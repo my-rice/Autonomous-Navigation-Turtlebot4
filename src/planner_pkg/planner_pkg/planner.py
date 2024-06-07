2096e9c7820193c506fa6833a6c444d9a5b0dde2
my-rice <dvdrsi2017@gmail.com>
2024-06-07 19:04:30 +0200

    bug2

--- src/planner_pkg/planner_pkg/planner.py
+++ src/planner_pkg/planner_pkg/planner.py
@@ -11,49 +11,27 @@ class InfoHandler(Node):
 
     def __init__(self):
         super().__init__("Info") #Init node
-        self.sub_odom = self.create_subscription(Odometry, "/odom", self.odom_callback, 10) # INUTILE
-        #self.sub_pose = self.create_subscription(PoseWithCovarianceStamped, "/initialpose", self.pose_callback, 10)
-        self.amcl_pose = self.create_subscription(PoseWithCovarianceStamped, "/amcl_pose", self.pose_callback, 10)
-        
-        self.pose = None
-        self.amcl_pose = None
-        self.odom = None
+        self.sub = self.create_subscription(PoseWithCovarianceStamped, "/amcl_pose", self.pose_callback, 10)
+        self.amcl_pose = None    
         self.finished = False
 
-        self.loop()
-    
+        self.navigator = TurtleBot4Navigator()
+        self.run()
+
     def pose_callback(self, msg):
         self.amcl_pose = msg
         self.get_logger().info(f"Pose: {msg.pose.pose.position.x}, {msg.pose.pose.position.y}")
 
-
     
-    def odom_callback(self, msg):
-        self.odom = msg
-        self.get_logger().info(f"Odometry: {msg.pose.pose.position.x}, {msg.pose.pose.position.y}")
-    
-
-
-    def loop(self):
-
-        navigator = TurtleBot4Navigator()
-
-        finished = False
-        
-        # Set initial pose
-        # initial_pose = navigator.getPoseStamped([0.0, 0.0], TurtleBot4Directions.NORTH) # TODO: The initial pose will be given dynamically
-        # navigator.setInitialPose(initial_pose)
-
-        # Wait for Nav2
-        #navigator.waitUntilNav2Active()
-
-
+    def run(self):
         while not self.finished:
             if self.amcl_pose is not None:
-                self.get_logger().info("self.acml_pose is not None", self.amcl_pose)
+                self.get_logger().info(f"Pose: {self.amcl_pose.pose.pose.position.x}, {self.amcl_pose.pose.pose.position.y}")
             else:
-                self.get_logger().info("Planning...")
-            time.sleep(2)
+                self.get_logger().info("Pose not available")
+            time.sleep(1)
+
+
 
 
 
@@ -64,7 +42,9 @@ def main(args=None):
 
     info = InfoHandler()
 
+
     executor.add_node(info)
+
     try:
         executor.spin()
     except KeyboardInterrupt:
