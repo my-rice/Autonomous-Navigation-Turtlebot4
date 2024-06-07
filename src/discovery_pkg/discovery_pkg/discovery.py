5231250f8934e98e1c95c8be0c5c9f6ccf6b1a49
giovannintonti <g.intonti@studenti.unisa.it>
2024-06-07 18:52:44 +0200

    [ADD] Discovery config

--- src/discovery_pkg/discovery_pkg/discovery.py
+++ src/discovery_pkg/discovery_pkg/discovery.py
@@ -1,40 +1,51 @@
 import rclpy
-from rclpy.lifecycle import State, TransitionCallbackReturn, Node
-from example_interfaces.srv import AddTwoInts
+from rclpy.node import Node
+from discovery_interface.srv import DiscoveryService
+from turtlebot4_navigation.turtlebot4_navigator import TurtleBot4Directions, TurtleBot4Navigator
+import yaml
+import math
 
 class Discovery(Node):
-    def __init__(self, node_name,**kwargs) -> None:
-        super().__init__(node_name,**kwargs)
-        self.srv = self.create_service(AddTwoInts, 'add_two_ints', self.add_two_ints_callback)
+    def __init__(self, node_name, **kwargs):
+        super().__init__(node_name, **kwargs)
+        self.srv = self.create_service(DiscoveryService, 'discovery_mode', self.discovery_mode_callback)
+        self.get_logger().info("Service 'discovery_mode' is ready")
+        self.navigator = TurtleBot4Navigator()
+        self.read_parameters()
+        self.founded = False
 
+    def read_parameters(self):
+        with open('config.yaml', 'r') as file:
+            config = yaml.safe_load(file)
 
-    def on_configure(self, state: State) -> TransitionCallbackReturn:
-        self.get_logger().info(f"Node '{self.get_name()}' is in state '{state.label}'. Transitioning to 'configure'")
-        return TransitionCallbackReturn.SUCCESS
+        self.policy = config['policy']['array_based']
+        self.radius = config['policy']['radius']
 
-    def on_activate(self, state: State) -> TransitionCallbackReturn:
-        self.get_logger().info(f"Node '{self.get_name()}' is in state '{state.label}'. Transitioning to 'activate'")
-        return TransitionCallbackReturn.SUCCESS
+    def discovery_mode_callback(self, request, response):
+        self.get_logger().info(f'Incoming request\na: {request.goal_pose_x} b: {request.goal_pose_y}')
+        self.founded = False
+        try:
+            self.navigator.startToPose(self.navigator.getPoseStamped([request.goal_pose_x, request.goal_pose_y], request.angle))
+        except Exception as e:
+            self.get_logger().error(f"An error occurred: {e}")
+            response.next_action = "error"
 
-    def on_deactivate(self, state: State) -> TransitionCallbackReturn:
-        self.get_logger().info(f"Node '{self.get_name()}' is in state '{state.label}'. Transitioning to 'deactivate'")
-        return TransitionCallbackReturn.SUCCESS
+        while not self.founded:
+            
 
-    def on_shutdown(self, state: State) -> TransitionCallbackReturn:
-        self.get_logger().info(f"Node '{self.get_name()}' is in state '{state.label}'. Transitioning to 'shutdown'")
-        return TransitionCallbackReturn.SUCCESS
-
-    def add_two_ints_callback(self, request, response):
-        response.sum = request.a + request.b
-        self.get_logger().info('Incoming request\na: %d b: %d' % (request.a, request.b))
+        
+        # Se il ciclo si conclude senza ricevere il segnale di "STOP action",
+        # imposta la prossima azione nella risposta come "not found"
+        if not self.founded:
+            response.next_action = "not found"
 
         return response
 
-def main(args=None) -> None:
-  rclpy.init(args=args)
-  lifecycle_node = Discovery("lifecycle_node")
-  rclpy.spin(lifecycle_node)
-  rclpy.shutdown()
+def main(args=None):
+    rclpy.init(args=args)
+    discovery_node = Discovery("discovery_node")
+    rclpy.spin(discovery_node)
+    rclpy.shutdown()
 
 if __name__ == "__main__":
-  main()
+    main()
