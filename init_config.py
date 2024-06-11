e78f5c73839dcd6fb278a167b36edcc11ff5fe98
my-rice <dvdrsi2017@gmail.com>
2024-06-11 23:06:19 +0200

    [ADD] automatic initialization configured

--- init_config.py
+++ init_config.py
@@ -1,3 +1,34 @@
+import getpass
+import subprocess
+
+# Ask for the password
+password = getpass.getpass("Enter your password: ")
+
+# Define the command
+command = 'sudo -S ntpdate ntp.ubuntu.com'
+
+# Run the command
+proc = subprocess.Popen(['sudo', '-S'] + command.split(), stdin=subprocess.PIPE, stderr=subprocess.PIPE)
+sudo_prompt = proc.communicate(password.encode())[1]
 
 
-commands = [ 'sudo ntpdate ntp.ubuntu.com; cd ./../scripts; ./configure_discovery.sh;']
+# Define the command
+command = 'cd ./../scripts; ./configure_discovery.sh'
+
+# Define the inputs
+inputs = '192.168.111.111\n\n\n'
+
+# Run the command
+proc = subprocess.Popen(command, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+stdout, stderr = proc.communicate(inputs.encode())
+
+print(stdout.decode())
+
+# Define the command
+command = 'ros2 daemon stop; ros2 daemon start; ros2 topic list'
+
+# Run the command
+proc = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+stdout, stderr = proc.communicate()
+
+print(stdout.decode())
