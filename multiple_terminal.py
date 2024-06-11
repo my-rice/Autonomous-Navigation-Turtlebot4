e53af5811f85b422afeb5bc6303612f520e41c05
giovannintonti <g.intonti@studenti.unisa.it>
2024-06-11 10:09:54 +0200

    [ADD] Node to python script

--- multiple_terminal.py
+++ multiple_terminal.py
@@ -5,12 +5,13 @@ subprocess.Popen(['gnome-terminal', '--', 'bash', '-c', 'colcon build;'])
 time.sleep(2)
 
 commands = [
-    'source install/setup.bash && ros2 launch diem_gazebo turtlebot4_ignition_no_dock.launch.py',
+    #'source install/setup.bash && ros2 launch diem_gazebo turtlebot4_ignition_no_dock.launch.py',
     'source install/setup.bash && ros2 launch turtlebot4_navigation localization.launch.py map:=src/map/diem_map.yaml',
     'source install/setup.bash && ros2 launch turtlebot4_navigation nav2.launch.py',
     'source install/setup.bash && ros2 launch turtlebot4_viz view_robot.launch.py',
     'source install/setup.bash && ros2 run planner_pkg planner',
-    'source install/setup.bash && ros2 run discovery_pkg discovery'
+    'source install/setup.bash && ros2 run discovery_pkg discovery',
+    'source install/setup.bash && ros2 run sig_rec decode_mod'
 ]
 
 for cmd in commands:
