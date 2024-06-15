0d7d54a0fe70cd72a78333ad765ceeb25709009c
my-rice <dvdrsi2017@gmail.com>
2024-06-15 14:11:47 +0200

    [FIX] all good now - only recovery need to be fixed

--- multiple_terminal.py
+++ multiple_terminal.py
@@ -9,7 +9,6 @@ commands = [
     'source install/setup.bash && ros2 launch turtlebot4_navigation localization.launch.py map:=src/map/diem_map.yaml',
     'source install/setup.bash && ros2 launch turtlebot4_navigation nav2.launch.py',
     'source install/setup.bash && ros2 launch turtlebot4_viz view_robot.launch.py',
-    'source install/setup.bash && ros2 run sig_rec decode_mod',
     'source install/setup.bash && ros2 run discovery_pkg discovery',
     'source install/setup.bash && ros2 launch planner_pkg planner_launch.py'
 ]
