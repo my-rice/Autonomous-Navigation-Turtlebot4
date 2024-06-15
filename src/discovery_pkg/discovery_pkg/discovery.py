1d5631e408ad725427efea44e8bfd783b2e94049
giovannintonti <g.intonti@studenti.unisa.it>
2024-06-15 10:01:23 +0200

    [FIX] Fixed discovery node

--- src/discovery_pkg/discovery_pkg/discovery.py
+++ src/discovery_pkg/discovery_pkg/discovery.py
@@ -10,6 +10,8 @@ from std_msgs.msg import String
 import math
 from geometry_msgs.msg import PoseWithCovarianceStamped
 import time
+from lifecycle_msgs.srv import ChangeState
+from rclpy.action import CancelResponse, GoalResponse
 
 class Discovery(Node):
     def __init__(self, node_name, config_path=None, **kwargs):
@@ -18,7 +20,9 @@ class Discovery(Node):
                     self,
                     DiscoveryAction,
                     'discovery_mode',    
-                    self.discovery_mode_callback)
+                    self.discovery_mode_callback,
+                    goal_callback=self.goal_callback,
+                    cancel_callback=self.cancel_callback)
         self.get_logger().info("Action Server 'discovery_mode' is ready")
         self.navigator = TurtleBot4Navigator()
         self.read_parameters(config_path)
@@ -28,11 +32,38 @@ class Discovery(Node):
         # Create a subscription for the /tests topic
         self.sub = self.create_subscription(String, '/output_command', self.signal_callback, 10)
         self.doublesub = self.create_subscription(PoseWithCovarianceStamped, "/amcl_pose", self.pose_callback, 10)
+        self.mutex = threading.Lock()
+        #PROBABILE ERRORE
+        self.service_up = False
+        self.client = self.create_client(ChangeState, 'lifecycle_perception/change_state')
+        self.wait_for_service()
+        self.req = ChangeState.Request()
+        self.req.transition.id = 1 
+        future = self.client.call_async(self.req)
+        rclpy.spin_until_future_complete(self, future)
+        #PROBABILE ERRORE
 
         self.get_logger().info("Subscribed to /output_command topic")
 
         self.nav_thread = None
+        self.cancel_requested = False
 
+    def goal_callback(self, goal_request):
+        self.get_logger().info('Received goal request')
+        return GoalResponse.ACCEPT
+    
+    def cancel_callback(self, goal_handle):
+        self.get_logger().info('Received cancel request')
+
+        with self.mutex:
+            if self.nav_thread is not None and self.nav_thread.is_alive():
+                self.get_logger().info('Cancelling navigation thread')
+                self.navigator.cancelTask()
+                #self.nav_thread.join()
+                self.get_logger().info('Navigation thread cancelled')
+                self.cancel_requested = True
+
+        return CancelResponse.ACCEPT
     def read_parameters(self, config_path):
         if config_path is None:
             config_path = 'src/discovery_pkg/config/config.yaml'
@@ -41,36 +72,22 @@ class Discovery(Node):
             config = yaml.safe_load(file)
 
         self.policy = config['policy']['array_based']
+        self.n_points = config['policy']['n_points']
+        self.spin_dist = config['policy']['spin_dist']
 
-    # def policy_arc(self, goal_x, goal_y, angle, start_x, start_y): # TODO : Aggiungere dove spostarsi prima in base all'incrocio
-    #     r = math.sqrt((goal_x - start_x)**2 + (goal_y - start_y)**2)
-    #     increment_x = (goal_x - start_x) / 4
-    #     increment_y = (goal_y - start_y) / 4
-    #     points = []
+    def wait_for_service(self):
+        if (not self.client.wait_for_service(timeout_sec=10.0)):
+            self.get_logger().info("Service is not ready, shutting down...")
+            rclpy.shutdown()
+            exit()
 
-    #     for i in range(4):
-    #         x = start_x + increment_x * i
-    #         y = start_y + increment_y * i
-    #         points.append((x, y, angle))
-
-    #     theta = math.atan2(goal_y - start_y, goal_x - start_x)
-    #     angles = [math.radians(angle) for angle in [22.5, 45, 67.5, -22.5, -45, -67.5]]
-
-    #     for angle_offset in angles:
-    #         arc_angle = theta + angle_offset
-    #         x = start_x + r * math.cos(arc_angle)
-    #         y = start_y + r * math.sin(arc_angle)
-    #         points.append((x, y, angle + angle_offset))
-    #     self.get_logger().info(f"Points: {points}")
-    #     return points
-    
     def policy_straight(self, goal_x, goal_y, angle, start_x, start_y, n_points=4):
         r = math.sqrt((goal_x - start_x)**2 + (goal_y - start_y)**2)
         increment_x = (goal_x - start_x) / n_points
         increment_y = (goal_y - start_y) / n_points
         points = []
 
-        for i in range(n_points):
+        for i in range(1,n_points):
             x = start_x + increment_x * i
             y = start_y + increment_y * i
             points.append((x, y, angle))
@@ -79,63 +96,109 @@ class Discovery(Node):
     def approach(self):
         raise NotImplementedError
 
-    def start_navigation(self, goal_x, goal_y, angle, start_x, start_y):
+    def start_navigation(self, goal_x, goal_y, angle, start_x, start_y, n_points=4):
         # points = self.policy_arc(goal_x, goal_y, angle, start_x, start_y)
-        points = self.policy_straight(goal_x, goal_y, angle, start_x, start_y)
+        points = self.policy_straight(goal_x, goal_y, angle, start_x, start_y, n_points)
         for point in points:
             self.get_logger().info(f"Point: {point}")
-            for i in range(-1,2):
+            for i in [-1,2,-1]:
                 try:
-                    self.navigator.startToPose(self.navigator.getPoseStamped((point[0], point[1]), point[2]+i*90))
+                    self.navigator.spin(spin_dist=math.radians(self.spin_dist*i))
+                    while not self.navigator.isTaskComplete():
+                        feedback = self.navigator.getFeedback()
                     if self.founded == True:
                         self.get_logger().info(f"Founded signal: {self.signal}")
                         break
                 except Exception as e:
                     self.get_logger().info(f"Error: {e}")
-                    self.signal = "Error"
+                    # self.signal = "Error"
                     break
-        if(self.signal is None):
-            #approach
-            approach = self.approach()
+            try:
+                self.navigator.goToPose(self.navigator.getPoseStamped((point[0], point[1]), point[2]))
+                while not self.navigator.isTaskComplete():
+                    feedback = self.navigator.getFeedback()
+                if self.founded == True:
+                    self.get_logger().info(f"Founded signal: {self.signal}")
+                    break
+            except Exception as e:
+                self.get_logger().info(f"Error: {e}")
+                # self.signal = "Error"
+                break
+
+        # if(self.signal is None):
+        #     #approach
+        #     approach = self.approach()
         self.get_logger().info("Navigation completed successfully")
 
     
 
     def discovery_mode_callback(self, goal_handle):
+        self.wait_for_service()
+        self.req.transition.id = 3
+        future = self.client.call_async(self.req)
+        future.add_done_callback(self.handle_service_response)
+        while not self.service_up:
+            self.get_logger().info("Waiting for service to be up")
+            time.sleep(0.1)
 
         goal = goal_handle.request
+        result = DiscoveryAction.Result()
+
         self.get_logger().info(f'Incoming request\n x: {goal.goal_pose_x} y: {goal.goal_pose_y} angle: {goal.angle} start_x: {goal.start_pose_x} start_y: {goal.start_pose_y}')
 
         self.founded = False
         self.signal = None
-
+        # self.start_navigation(goal.goal_pose_x, goal.goal_pose_y, goal.angle, goal.start_pose_x, goal.start_pose_y)
+       
         # Start the navigation in a separate thread
-        self.nav_thread = threading.Thread(target=self.start_navigation, args=(goal.goal_pose_x, goal.goal_pose_y, goal.angle, goal.start_pose_x, goal.start_pose_y))
-        self.nav_thread.start()
+        with self.mutex:
+            self.nav_thread = threading.Thread(target=self.start_navigation, args=(goal.goal_pose_x, goal.goal_pose_y, goal.angle, goal.start_pose_x, goal.start_pose_y, self.n_points))
+            self.nav_thread.start()
 
         # Wait for the navigation to complete, allowing other callbacks to be processed
         self.nav_thread.join()  # Wait until the navigation thread completes
+        self.nav_thread = None
+        self.wait_for_service()
+        self.req.transition.id = 4
+        future = self.client.call_async(self.req)
+        future.add_done_callback(self.handle_service_response)
+        if self.cancel_requested:
+            goal_handle.abort()
+            self.cancel_requested = False
+            result.next_action = "Canceled"
+            return result
+        else:
+            # if(self.signal=='None'):
+            #     result.next_action = "None"
+            # elif(self.signal is not None):
+            #     result.next_action = self.signal
+            # else:
+            #     result.next_action = "Error"
+            if(self.signal is None):
+                result.next_action = "right" # TODO Da fare random
+            else:
+                result.next_action = (str(self.signal)).lower()
+                    
+            self.get_logger().info("result next action: " + result.next_action)
+            return result
 
-        self.get_logger().info("Discovery mode callback")
-        result = DiscoveryAction.Result()
-        self.get_logger().info("Discovery mode callback 2")
-        goal_handle.succeed()
-        self.get_logger().info("Discovery mode callback 3")
-        # if(self.signal=='None'):
-        #     result.next_action = "None"
-        # elif(self.signal is not None):
-        #     result.next_action = self.signal
-        # else:
-        #     result.next_action = "Error"
-        result.next_action = "right"
-        return result
-
+    def handle_service_response(self, future):
+    # Questa funzione viene chiamata quando la future è completata
+        response = future.result()
+        if response.success:
+            self.get_logger().info("Operazione di servizio completata con successo")
+            self.service_up=True
+        else:
+            self.get_logger().info("Operazione di servizio fallita")
+            self.service_up=False
+        # Qui puoi aggiungere ulteriore logica per gestire la risposta
 
     def signal_callback(self, msg):
         self.get_logger().info(f'Received signal: {msg.data}')
         if msg.data == "None":
-            self.founded = True
-            self.navigator.cancelTask()
+            # self.founded = True
+            # self.navigator.cancelTask()
+            pass
         elif msg.data != "No code":
             self.signal = msg.data  
             self.founded = True
@@ -146,7 +209,6 @@ class Discovery(Node):
 
     def pose_callback(self, msg):
         self.amcl_pose = msg
-        self.get_logger().info(f"Pose callback: {msg.pose.pose.position.x}, {msg.pose.pose.position.y}")
         
 def main(args=None):
     rclpy.init(args=args)
